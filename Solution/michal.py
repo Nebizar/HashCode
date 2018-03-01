bf5e08d798ab222199b68e25e78c640dfbd121f5
Zofia Tarant <zosiarrf@gmail.com>
2018-03-01 20:41:43 +0100

    aaa

--- Solution/michal.py
+++ Solution/michal.py
@@ -7,18 +7,21 @@ class Order:
     B=[]
     start = 0
     end = 0
+
     def __init__(self, inId, inA, inB, inStart, inEnd):
         self.A = inA
         self.B = inB
         self.start = inStart
         self.end = inEnd
         self.id = inId
+
     def printInfo(self):
         print(self.id, self.A, self.B, self.start, self.end)
 
 
 class OrderManager:
     orders = []
+
     def loadFromFile(self, path):
         input = fileinput.input(path)
         i=0
@@ -27,13 +30,13 @@ class OrderManager:
             order = Order(i, [Ax, Ay], [Bx, By], start, end)
             self.orders.append( order )
             i += 1
+
     def printInfo(self):
         for order in self.orders:
             print(order.printInfo())
 
-
-orderManager = OrderManager()
+"""orderManager = OrderManager()
 orderManager.loadFromFile("a_example.in")
 orderManager.printInfo()
 
-
+"""
